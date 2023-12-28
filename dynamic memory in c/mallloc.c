b49ce0b64dbf0978a9e24de094dd5959e04e6b17
Darkx-69 <142841709+Darkx-69@users.noreply.github.com>
2023-12-28 20:09:20 +0530

    new things

---
+++ dynamic memory in c/mallloc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int *ptr;
+    int n;
+    printf("enter the size of the array\n ");
+    scanf("%d", &n);
+
+    ptr = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("The element %d of the array is %d \n", i, ptr[i]);
+    }
+printf("The element %d of the array is %d \n", 3, ptr[3]);
+    return 0;
+}
---
+++ dynamic memory in c/quiz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int *ptr;
+    int n;
+    printf("enter the size of the array\n ");
+    scanf("%d", &n);
+
+    ptr = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("The element %d of the array is %d \n", i, ptr[i]);
+    }
+    free(ptr);
+     printf("enter the size of the array\n ");
+    scanf("%d", &n);
+        
+    ptr=(int*)malloc(n*sizeof(int));
+for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("The element %d of the array is %d \n", i+1, ptr[i]);
+    }
+    free(ptr);
+
+    return 0;
+}
